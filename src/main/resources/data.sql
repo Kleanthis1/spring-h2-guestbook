bf01b2ab69fc51a026217dbffead438fc58eda94
TonyJenkins <tony.jenkins@elder-studios.co.uk>
2017-08-31 10:58:46 +0100

    Initial Commit with H2 Settings.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,24 +1,24 @@
-insert ignore into entries (entry_id, user, comment, created_date, modified_date)
-              values (1, 'Catrin Morgan', 'Nice use of contrast in this colour palette.', now(), now());
-insert ignore into entries (entry_id, user, comment, created_date, modified_date)
-              values (2, 'Cletus Spalding', 'It''s splendid not just sleek!', now(), now());
-insert ignore into entries (entry_id, user, comment, created_date, modified_date)
-              values (3, 'Grigor Martel', 'This is alluring work! ', now(), now());
-insert ignore into entries (entry_id, user, comment, created_date, modified_date)
-              values (4, 'Finnegan Draper', 'Super thought out! Leading the way, mate.', now(), now());
-insert ignore into entries (entry_id, user, comment, created_date, modified_date)
-              values (5, 'Zac Quincy', 'My 49 year old daughter rates this shot very splendid :)', now(), now());
-insert ignore into entries (entry_id, user, comment, created_date, modified_date)
-              values (6, 'Grigor Martel', 'Such shot, many button, so simple.', now(), now());
-insert ignore into entries (entry_id, user, comment, created_date, modified_date)
-              values (7, 'Terrence Sutherland', 'Looks revolutionary and admirable, friend.', now(), now());
-insert ignore into entries (entry_id, user, comment, created_date, modified_date)
-              values (8, 'Terrence Sutherland', 'I want to learn this kind of shot! Teach me.', now(), now());
-insert ignore into entries (entry_id, user, comment, created_date, modified_date)
-              values (9, 'Zac Quincy', 'Incredible work you have here.', now(), now());
-insert ignore into entries (entry_id, user, comment, created_date, modified_date)
-              values (10, 'Grigor Martel', 'Engaging. It keeps your mind occupied while you wait.', now(), now());
-insert ignore into entries (entry_id, user, comment, created_date, modified_date)
-              values (11, 'Katharine McKenzie', 'White. This is new school.', now(), now());
-insert ignore into entries (entry_id, user, comment, created_date, modified_date)
-              values (12, 'Zac Quincy', 'I approve your shot.', now(), now());
+insert into entries (entry_id, user, comment, created_date, modified_date)
+       values (1, 'Catrin Morgan', 'Nice use of contrast in this colour palette.', current_timestamp(), current_timestamp());
+insert into entries (entry_id, user, comment, created_date, modified_date)
+       values (2, 'Cletus Spalding', 'It''s splendid not just sleek!', current_timestamp(), current_timestamp());
+insert into entries (entry_id, user, comment, created_date, modified_date)
+       values (3, 'Grigor Martel', 'This is alluring work! ', current_timestamp(), current_timestamp());
+insert into entries (entry_id, user, comment, created_date, modified_date)
+       values (4, 'Finnegan Draper', 'Super thought out! Leading the way, mate.', current_timestamp(), current_timestamp());
+insert into entries (entry_id, user, comment, created_date, modified_date)
+       values (5, 'Zac Quincy', 'My 49 year old daughter rates this shot very splendid :)', current_timestamp(), current_timestamp());
+insert into entries (entry_id, user, comment, created_date, modified_date)
+       values (6, 'Grigor Martel', 'Such shot, many button, so simple.', current_timestamp(), current_timestamp());
+insert into entries (entry_id, user, comment, created_date, modified_date)
+       values (7, 'Terrence Sutherland', 'Looks revolutionary and admirable, friend.', current_timestamp(), current_timestamp());
+insert into entries (entry_id, user, comment, created_date, modified_date)
+       values (8, 'Terrence Sutherland', 'I want to learn this kind of shot! Teach me.', current_timestamp(), current_timestamp());
+insert into entries (entry_id, user, comment, created_date, modified_date)
+       values (9, 'Zac Quincy', 'Incredible work you have here.', current_timestamp(), current_timestamp());
+insert into entries (entry_id, user, comment, created_date, modified_date)
+       values (10, 'Grigor Martel', 'Engaging. It keeps your mind occupied while you wait.', current_timestamp(), current_timestamp());
+insert into entries (entry_id, user, comment, created_date, modified_date)
+       values (11, 'Katharine McKenzie', 'White. This is new school.', current_timestamp(), current_timestamp());
+insert into entries (entry_id, user, comment, created_date, modified_date)
+       values (12, 'Zac Quincy', 'I approve your shot.', current_timestamp(), current_timestamp());
